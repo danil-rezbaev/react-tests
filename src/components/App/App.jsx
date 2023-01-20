f4d11017c8c8d43bb85b857746edaa779bdf77a3
danil-rezbaev <92051977+danil-rezbaev@users.noreply.github.com>
2023-01-20 23:49:53 +0300

    rewritten react testing app

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,31 +1,115 @@
-import './App.css';
-import List from "../List";
-import { useCallback, useMemo, useState } from "react";
-import Search from "../Search/Search";
-
-const data = ['html', 'css', 'javascript', 'typescript', 'React']
+import './App.scss';
+import { useCallback, useState } from "react";
+import { Button, Col, Container, Form, Row } from "react-bootstrap";
 
 function App() {
-  const [search, setSearch] = useState()
+  const defaultValue = {
+    operator: '+'
+  }
 
-  const filterItem = useMemo(() => {
-    return data.filter(item => item.startsWith(search))
-  }, [search])
+  const [formData, setFormData] = useState(defaultValue)
+  const [result, setResult] = useState(undefined)
 
-  const onChange = useCallback((event) => {
-    setSearch(event.target.value)
+  const onChangeField = useCallback((event) => {
+    const { name, value } = event.target
+
+    setFormData((data) => ({
+        ...data,
+        [name]: value
+      }))
   }, [])
+
+  const calculate = useCallback(() => {
+    const {operator, firstArg, secondArg} = formData
+
+    console.log('formData', formData)
+
+    if (!firstArg || !secondArg || !operator) {
+      return 0
+    }
+
+    if (typeof +firstArg !== "number" || typeof +secondArg !== "number") {
+      return 0
+    }
+
+    if(operator === '+') {
+      return +firstArg + +secondArg
+    } else if(operator === '-') {
+      return +firstArg - +secondArg
+    } else if(operator === '*') {
+      return +firstArg * +secondArg
+    }
+  }, [formData])
+
+  const buttonClick = useCallback((event) => {
+    const form = event.currentTarget;
+
+    event.preventDefault();
+    event.stopPropagation();
+
+    if (form.checkValidity() === true) {
+      setResult(calculate())
+    }
+  }, [calculate])
 
   return (
     <div className="App">
-      <header className="App-header">
-        <Search
-          value={search}
-          onChange={onChange}>
-          Find:
-        </Search>
-        <List items={filterItem}/>
-      </header>
+      <Container className="mt-5">
+        <Row>
+          <Col>
+            <Form.Control
+              placeholder="аргумент 1"
+              name="firstArg"
+              onChange={onChangeField}
+              value={formData["firstArg"] ?? ''}
+            />
+          </Col>
+
+          <Col>
+            <Form.Select
+              name="operator"
+              onChange={onChangeField}
+              aria-label="оператор"
+              value={formData["operator"] ?? '+'}
+              role="listbox"
+              required
+            >
+              <option value="+">+</option>
+              <option value="-">-</option>
+              <option value="*">*</option>
+            </Form.Select>
+          </Col>
+
+          <Col>
+            <Form.Control
+              placeholder="аргумент 2"
+              name="secondArg"
+              onChange={onChangeField}
+              value={formData["secondArg"] ?? ''}
+              className="form-control"
+            />
+          </Col>
+
+          <Col>
+            { typeof result !== 'undefined'
+              ? <h4 className="result text-start">= {result}</h4>
+              : null
+            }
+          </Col>
+
+          <Col md={12} className="mt-2">
+            <Button
+              type="submit"
+              variant="primary"
+              className="text-white w-100"
+              role="submitButton"
+              onClick={buttonClick}
+            >
+              Рассчитать!
+            </Button>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
--- src/components/App/App.test.jsx
+++ src/components/App/App.test.jsx
@@ -7,23 +7,120 @@ describe("App component", () => {
     render(<App />);
   });
 
-  test('typing in Searchbox works', () => {
-    render(<App />);
-
-    expect(screen.queryByDisplayValue(/React/)).toBeNull()
-
-    userEvent.type(screen.getByRole('textbox'), 'React')
-
-    expect(screen.getByDisplayValue(/React/)).toBeInTheDocument()
+  test('app component render with unnecessary props', () => {
+    render(<App data="prop" />);
   });
 
-  test('search filter is working', () => {
+  test('result default be null', () => {
+    render(<App />);
+    expect(screen.queryByText(/=/)).toBeNull()
+  });
+
+  test('fields are on the page', () => {
+    const { container } = render(<App />);
+
+    // eslint-disable-next-line testing-library/no-container, testing-library/no-node-access
+    const formСontrols = container.querySelectorAll('.form-control');
+    // eslint-disable-next-line testing-library/no-container, testing-library/no-node-access
+    const formSelects = container.querySelectorAll('.form-select');
+
+    expect(formСontrols.length).toBe(2);
+    expect(formSelects.length).toBe(1);
+  });
+
+  test('submit button are on the page', () => {
+    const { container } = render(<App />);
+
+    // eslint-disable-next-line testing-library/no-container, testing-library/no-node-access
+    const buttonSubmit = container.querySelector('button[type="submit"]');
+
+    expect(buttonSubmit).toBeInTheDocument()
+  });
+
+  test('the value of the .form-control changes when typing', () => {
+    const { container } = render(<App />);
+    const textToType = '10'
+
+    // eslint-disable-next-line testing-library/no-container, testing-library/no-node-access
+    const firstArg = container.querySelector(`input[name="firstArg"]`);
+
+    userEvent.type(firstArg, textToType)
+    expect(firstArg).toHaveAttribute('value', textToType)
+  });
+
+  test('test select has default value', () => {
+    render(<App />);
+    expect(screen.getByText("+")).toBeInTheDocument();
+  });
+
+  test('changing select values works as expected', () => {
     render(<App />);
 
-    userEvent.type(screen.getByRole('textbox'), 'java')
+    userEvent.selectOptions(screen.getByRole('listbox'), ['*'])
 
-    expect(screen.queryByText(/React/)).toBeNull()
-    expect(screen.getByText(/javascript/)).toBeInTheDocument()
+    expect(screen.getByRole('option', {name: '+'}).selected).toBe(false)
+    expect(screen.getByRole('option', {name: '-'}).selected).toBe(false)
+    expect(screen.getByRole('option', {name: '*'}).selected).toBe(true)
+  });
+
+  test('testing the addition of numbers', () => {
+    const { container } = render(<App />);
+    const textToType = '10'
+    userEvent.selectOptions(screen.getByRole('listbox'), ['+'])
+
+    // eslint-disable-next-line testing-library/no-container, testing-library/no-node-access
+    const firstArg = container.querySelector(`input[name="firstArg"]`);
+    userEvent.type(firstArg, textToType)
+
+    // eslint-disable-next-line testing-library/no-container, testing-library/no-node-access
+    const secondArg = container.querySelector(`input[name="secondArg"]`);
+    userEvent.type(secondArg, textToType)
+
+    // eslint-disable-next-line testing-library/no-container, testing-library/no-node-access
+    const buttonSubmit = container.querySelector(`button[type="submit"]`);
+    userEvent.click(buttonSubmit)
+
+    expect(screen.getByText(/20/)).toBeInTheDocument();
+  });
+
+  test('testing the subtraction of numbers', () => {
+    const { container } = render(<App />);
+    const textToType = '10'
+    userEvent.selectOptions(screen.getByRole('listbox'), ['-'])
+
+    // eslint-disable-next-line testing-library/no-container, testing-library/no-node-access
+    const firstArg = container.querySelector(`input[name="firstArg"]`);
+    userEvent.type(firstArg, textToType)
+
+    // eslint-disable-next-line testing-library/no-container, testing-library/no-node-access
+    const secondArg = container.querySelector(`input[name="secondArg"]`);
+    userEvent.type(secondArg, textToType)
+
+    // eslint-disable-next-line testing-library/no-container, testing-library/no-node-access
+    const buttonSubmit = container.querySelector(`button[type="submit"]`);
+    userEvent.click(buttonSubmit)
+
+    expect(screen.getByText(/0/)).toBeInTheDocument();
+  });
+
+  test('testing the multiplication of numbers', () => {
+    const { container } = render(<App />);
+    const textToType = '10'
+    userEvent.selectOptions(screen.getByRole('listbox'), ['*'])
+
+    // eslint-disable-next-line testing-library/no-container, testing-library/no-node-access
+    const firstArg = container.querySelector(`input[name="firstArg"]`);
+    userEvent.type(firstArg, textToType)
+
+    // eslint-disable-next-line testing-library/no-container, testing-library/no-node-access
+    const secondArg = container.querySelector(`input[name="secondArg"]`);
+    userEvent.type(secondArg, textToType)
+
+    // eslint-disable-next-line testing-library/no-container, testing-library/no-node-access
+    const buttonSubmit = container.querySelector(`button[type="submit"]`);
+    userEvent.click(buttonSubmit)
+
+    expect(screen.getByText(/100/)).toBeInTheDocument();
   });
 
   test('App snapshot', () => {
